00d759497a3dd614f9c19a1da8977bd563e51552
Marc Dunand <marcdunandc@gmail.com>
2025-11-14 10:59:46 -0500

    working on new color mapping

--- generateMapping/generateLayeredMapping.py
+++ generateMapping/generateLayeredMapping.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 def main():
     ap = argparse.ArgumentParser()
@@ -18,6 +19,23 @@ def main():
     for i in range(N+1):  #generally 0-24
         num1s = bin(i).count("1")
         layers[num1s].append(i)
+
+    mapping = []
+    for n in range(0, 3*side):
+        for x in range(0, side):
+            # Compute allowable y range
+            y_min = max(0, n - x - (side-1))
+            y_max = min(side-1, n - x)
+
+            if y_min > y_max:
+                continue  # no valid y for this x
+
+            for y in range(y_min, y_max + 1):
+                z = n - x - y  # guaranteed 0 <= z < m
+                mapping.append((x, y, z))
+
+
+    print(mapping[0:100])
         
 
 if __name__=="__main__":
